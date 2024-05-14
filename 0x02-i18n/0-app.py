9bd9519919289e59e9896f7d3e86fa8a4a164591
mouad mennioui <moad.menniui@gmail.com>
2024-05-14 15:34:27 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-"""the Basic Flask app.
+#!/usr/bin/env python3
+"""A Basic Flask app.
 """
 from flask import Flask, render_template
 
@@ -9,7 +9,7 @@ app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def index():
+def get_index() -> str:
     """The home/index page.
     """
     return render_template('0-index.html')
