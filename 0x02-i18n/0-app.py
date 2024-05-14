52cb097672144c8f6b3d8e5cd8ba2f5e6cecf937
mouad mennioui <moad.menniui@gmail.com>
2024-05-14 15:29:46 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,11 +3,14 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    """The home/index page.
+    """
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
